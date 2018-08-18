c17a50ddcf1aee01c3a5533b33ffeec3a95c99a5
matueranet <git@matuera.net>
2018-08-18 13:58:40 +0200

    Changed promises to async/await, moved to @genie-ai npm organisation. Updated outdated packages.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-const client = require('./lib/client')
+process.env.NTBA_FIX_319 = 1; // suppress the deprecation warning (https://github.com/yagop/node-telegram-bot-api/issues/319)
+const client = require('./lib/client');
 
 module.exports = {
-  client: client,
-  setRouter: client.setRouter.bind(client),
-}
+    client,
+    setRouter: client.setRouter.bind(client),
+};
